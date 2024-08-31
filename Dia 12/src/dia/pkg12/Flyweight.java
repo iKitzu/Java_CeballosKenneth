dac02e175f66fd5e0ee3be209ef557f17c3736d4
iKitzu <kennethsantiagoc@gmail.com>
2024-08-31 11:16:36 -0500

    fix: :fire: Winko

--- Dia 12/src/dia/pkg12/Flyweight.java
+++ Dia 12/src/dia/pkg12/Flyweight.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package dia.pkg12;
 
-/**
- *
- * @author Juan Felipe Rubio
- */
 
 import java.util.HashMap;
 import java.util.Map;
--- Dia 12/src/dia/pkg12/Main.java
+++ Dia 12/src/dia/pkg12/Main.java
@@ -1,13 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package dia.pkg12;
 
-/**
- *
- * @author Juan Felipe Rubio
- */
 
 /*
 public class Main {
